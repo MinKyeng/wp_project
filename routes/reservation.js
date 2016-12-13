109614c2e990d98f1fed87158c428356992986f7
minnn <minnn>
2016-12-13 13:29:49 +0900

    예약목록 error 수정

--- routes/reservation.js
+++ routes/reservation.js
@@ -56,7 +56,7 @@ router.post('/', function(req, res, next) {
       if (err) {
         return next(err);
       } else {
-        res.redirect('/reservation/index');
+        res.redirect('/reservation');
       }
     });
   });
